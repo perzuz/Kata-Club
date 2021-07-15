43aae9e73cdf0adc1b393415e1f1660828ec9324
HarrisScott <harrisscott@outlook.com>
2021-07-15 17:51:40 +0100

    Initial solution commit
    Add: Initial Kata-Club solution

---
+++ Kata-Club/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata_Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
