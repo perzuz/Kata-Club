f0bd7009fc6411a78f24e957b71a83543eca68ca
HarrisScott <harrisscott@outlook.com>
2021-07-29 22:07:18 +0100

    Add: initial Tic Tac Toe tests
    Add: initial Tic Tac Toe class

---
+++ Kata-Club/Katas/TicTacToe/TicTacToe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata_Club.Katas.TicTacToe
+{
+    // https://www.codewars.com/kata/525caa5c1bf619d28c000335/train/csharp
+    public class TicTacToe
+    {
+        public int IsSolved(int[,] board)
+        {
+            return -1;
+        }
+
+        private bool hasRow(int piece, List<int> row)
+        {
+            return row.Count(x => x == piece) == 3;
+        }
+
+    }
+}
---
+++ KataTests/TicTacToeTests.cs
@@ -0,0 +1,50 @@
+﻿using Kata_Club.Katas.TicTacToe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        [TestMethod]
+        public void GivenBoardNotYetFinished_ReturnNegativeOne()
+        {
+            // Arrange
+            var ticTacToe = new TicTacToe();
+
+            int[,] board = new int[,] {
+            { 1, 1, 0 },
+            { 0, 2, 2 },
+            { 0, 0, 0 } };
+
+            // Act
+            var actual = ticTacToe.IsSolved(board);
+
+            // Assert
+            var expected = -1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenXwins_ReturnOne()
+        {
+            // Arrange
+            var ticTacToe = new TicTacToe();
+
+            int[,] board = new int[,] {
+            { 1, 1, 1 },
+            { 0, 2, 2 },
+            { 0, 0, 0 } };
+
+            // Act
+            var actual = ticTacToe.IsSolved(board);
+
+            // Assert
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
