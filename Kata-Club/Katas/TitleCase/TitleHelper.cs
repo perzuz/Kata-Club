124cfdc242a0ec48718261924f2aea8f83c6150e
HarrisScott <harrisscott@outlook.com>
2021-08-04 08:31:50 +0100

    Add: TitleCase kata solution
    Add: TitleCase kata tests

---
+++ Kata-Club/Katas/TitleCase/TitleHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata_Club.Katas.TitleCase
+{
+    public class TitleHelper
+    {
+        public static string TitleCase(string title, string minorWords = "")
+        {
+            if (string.IsNullOrEmpty(title)) return "";
+
+            var minorWordsList = minorWords != null ? minorWords.Split(" ").ToList() : new List<string>();
+            var titleArr = title.Split(" ");
+
+            for (int i = 0; i < minorWordsList.Count; i++)
+            {
+                minorWordsList[i] = minorWordsList[i].ToLower();
+            }
+
+            for (int i = 0; i < titleArr.Length; i++)
+            {
+                var titleWord = titleArr[i] = titleArr[i].ToLower();
+
+                if (!minorWordsList.Contains(titleWord) || i == 0)
+                {
+                    titleArr[i] = Capatlise(titleWord);
+                }
+            }
+
+            return string.Join(" ", titleArr);
+        }
+
+        private static string Capatlise(string input)
+        {
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+}
---
+++ KataTests/TitleCaseTests.cs
@@ -0,0 +1,83 @@
+﻿using Kata_Club.Katas.TitleCase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+    // https://www.codewars.com/kata/5202ef17a402dd033c000009/train/csharp
+    [TestClass]
+    public class TitleCaseTests
+    {
+        [TestMethod]
+        public void SimpleTitle_WithListOfExceptionWords_ExpectedTitleCaseReturned()
+        {
+            // Arrange
+            var inputTitle = "a clash of KINGS";
+            var exceptionWords = "a an the of";
+
+            // Act
+            var actual = TitleHelper.TitleCase(inputTitle, exceptionWords);
+
+            // Assert
+            var expected = "A Clash of Kings";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AllCapsTitle_WithCapatalisedExceptionWords_ExpectedTitleCaseReturned()
+        {
+            // Arrange
+            var inputTitle = "THE WIND IN THE WILLOWS";
+            var exceptionWords = "The In";
+
+            // Act
+            var actual = TitleHelper.TitleCase(inputTitle, exceptionWords);
+
+            // Assert
+            var expected = "The Wind in the Willows";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AllLowerTitle_WithNoExceptionWords_ExpectedTitleCaseReturned()
+        {
+            // Arrange
+            var inputTitle = "the quick brown fox";
+            
+            // Act
+            var actual = TitleHelper.TitleCase(inputTitle);
+
+            // Assert
+            var expected = "The Quick Brown Fox";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmptyInput_WithNoExceptionWords_ReturnEmptyString()
+        {
+            // Arrange
+            var inputTitle = "";
+
+            // Act
+            var actual = TitleHelper.TitleCase(inputTitle);
+
+            // Assert
+            var expected = "";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SimpleInput_WithNullExceptionWords_ExpectedTitleCaseReturned()
+        {
+            // Arrange
+            var inputTitle = "aBC deF Ghi";
+            string exceptionWords = null;
+
+            // Act
+            var actual = TitleHelper.TitleCase(inputTitle, exceptionWords);
+
+            // Assert
+            var expected = "Abc Def Ghi";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
