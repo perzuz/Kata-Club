725e529f0b8dad10fcb549abfb6f59185cd7482d
HarrisScott <harrisscott@outlook.com>
2021-08-05 08:32:38 +0100

    Add: player movement
    Add: player turns functionality
    
    Add: roll double functionality

---
+++ Kata-Club/Katas/SnakesAndLadders/SnakesLadders.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata_Club.Katas.SnakesAndLadders
+{
+    // https://www.codewars.com/kata/587136ba2eefcb92a9000027/train/csharp
+    public class SnakesLadders
+    {
+        public SnakesLadders()
+        {
+            InitialisePlayers();
+        }
+
+        public string play(int die1, int die2)
+        {
+            MovePlayer(_playerTurn, die1 + die2);
+
+            var gameState = _players[_playerTurn].GetGameState();
+
+            if (die1 == die2) return gameState;
+
+            if (_playerTurn < _players.Count - 1)
+            {
+                _playerTurn++;
+            }
+            else
+            {
+                _playerTurn = 0;
+            }
+
+            return gameState;
+        }
+
+        public Player GetPlayer(int index)
+        {
+            return _players[index];
+        }
+
+        private List<Player> _players;
+        private int _playerTurn;
+
+        private void MovePlayer(int playerIndex, int numberOfPositions)
+        {
+            _players[playerIndex].Position += numberOfPositions;
+        }
+
+        private void InitialisePlayers()
+        {
+            _players = new List<Player>();
+
+            var numberOfPlayers = 2;
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                _players.Add(new Player($"Player {i + 1}"));
+            }
+        }
+    }
+
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public int Position { get; set; }
+        public string Name { get; private set; }
+
+        public string GetGameState()
+        {
+            return $"{Name} is on square {Position}";
+        }
+    }
+}
---
+++ KataTests/SnakesLaddersTests.cs
@@ -0,0 +1,105 @@
+﻿using Kata_Club.Katas.SnakesAndLadders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestClass]
+    public class SnakesLaddersTests
+    {
+        [TestMethod]
+        public void ANewGameStarts_BothPlayersAreOnSquareZero()
+        {
+            // Arrange
+            SnakesLadders game = new SnakesLadders();
+
+            // Act
+            var player1 = game.GetPlayer(0);
+            var player2 = game.GetPlayer(1);
+
+            // Assert
+            var expectedStartPosition = 0;
+            Assert.AreEqual(expectedStartPosition, player1.Position);
+            Assert.AreEqual(expectedStartPosition, player2.Position);
+        }
+
+        [TestMethod]
+        public void ANewGameStarts_FirstRoll_PlayerOneMovesToExpectedPosition()
+        {
+            // Arrange
+            SnakesLadders game = new SnakesLadders();
+
+            // Act
+            var actual = game.play(2, 1);
+
+            // Assert
+            var expectedGameOutput = "Player 1 is on square 3";
+            Assert.AreEqual(expectedGameOutput, actual);
+        }
+
+        [TestMethod]
+        public void ANewGameStarts_AfterTwoRolls_BothPlayersAreOnExpectedPositions()
+        {
+            // Arrange
+            SnakesLadders game = new SnakesLadders();
+
+            // Act
+            var actualRound1 = game.play(2, 1);
+            var actualRound2 = game.play(3, 1);
+
+            // Assert
+            var expectedRound1Output = "Player 1 is on square 3";
+            Assert.AreEqual(expectedRound1Output, actualRound1);
+
+            var expectedRound2Output = "Player 2 is on square 4";
+            Assert.AreEqual(expectedRound2Output, actualRound2);
+        }
+
+        [TestMethod]
+        public void ANewGameStarts_AfterThreeRolls_BothPlayersAreOnExpectedPositions()
+        {
+            // Arrange
+            SnakesLadders game = new SnakesLadders();
+
+            // Act
+            var actualRound1 = game.play(2, 1);
+            var actualRound2 = game.play(3, 1);
+            var actualRound3 = game.play(4, 2);
+
+            // Assert
+            var expectedRound1Output = "Player 1 is on square 3";
+            Assert.AreEqual(expectedRound1Output, actualRound1);
+
+            var expectedRound2Output = "Player 2 is on square 4";
+            Assert.AreEqual(expectedRound2Output, actualRound2);
+
+            var expectedRound3Output = "Player 1 is on square 9";
+            Assert.AreEqual(expectedRound3Output, actualRound3);
+        }
+
+        [TestMethod]
+        public void ANewGameStarts_PlayerRollsDouble_PlayerGoesAgain()
+        {
+            // Arrange
+            SnakesLadders game = new SnakesLadders();
+
+            // Act
+            var actualRound1 = game.play(2, 2);
+            var actualRound2 = game.play(4, 1);
+
+            var actualRound3 = game.play(4, 2);
+
+            // Assert
+            var expectedRound1Output = "Player 1 is on square 4";
+            Assert.AreEqual(expectedRound1Output, actualRound1);
+
+            var expectedRound2Output = "Player 1 is on square 9";
+            Assert.AreEqual(expectedRound2Output, actualRound2);
+
+            var expectedRound3Output = "Player 2 is on square 6";
+            Assert.AreEqual(expectedRound3Output, actualRound3);
+        }
+    }
+}
